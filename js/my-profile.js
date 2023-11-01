3a95769a5db7ac28a82180e13bb08079ed4ff807
maxipz <maximilianopz96@gmail.com>
2023-11-01 14:31:24 -0300

    Entrega 6 ,pauta 1 completa
    Falta guardar en localStorage

--- js/my-profile.js
+++ js/my-profile.js
@@ -26,3 +26,24 @@ document.getElementById('saveProfile').addEventListener('click', (e) => {
         } 
     });
 });
+
+// Al seleccionar una imagen que se agregue a un container y remplaze el placeholder por la nueva imagen
+document.getElementById("profileImgInput").addEventListener("change", function (event) {
+  const selectImg = document.querySelector("selectImg");    // lo utilizaremos para mostrar el nombre del archivo seleccionado
+  const profileImg = document.getElementById("profileImg");  // lo usamos para mostrar la vista previa de la imagen
+
+  if (event.target.files.length > 0) {
+    const selectedImage = event.target.files[0];    
+    const imageURL = URL.createObjectURL(selectedImage); // Crear una URL única que apunta a la imagen seleccionada y almacenarla en la variable imageURL
+
+    // Establecer el tamaño máximo de la imagen en la vista previa
+    profileImg.style.maxWidth = "150px";
+    profileImg.style.maxHeight = "200px";
+    
+    profileImg.src = imageURL;
+    selectImg.innerText = selectedImage.name;
+  } else {
+    profileImg.src = "img/img_perfil.png";
+    
+  }
+});
