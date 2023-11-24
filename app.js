ddc80e321f476a59a08694a38a254dc8195969ba
maxipz <maximilianopz96@gmail.com>
2023-11-23 22:18:44 -0300

    Segunda parte
    a

--- app.js
+++ app.js
@@ -1,19 +1,31 @@
-const express = require("express"); 
+const express = require("express");
+const cors  = require("cors");
+
+
+const cats = require('./basededatos/cats/cat.json');
+
+
+
+
 
 const app = express();
 const port = 3000;
 
+
+
 app.use(express.json()); 
+app.use(cors());
 
 
 app.get("/", (req, res) => {
   res.send("<h1>Bienvenid@ al servidor</h1>");
 });
 
-let cats = require('./basededatos/cats/cat.json')
+
 app.get('/cats', (req, res)=>{
-  res.send("<h1>Bienvenid@ al servidor</h1>");
+  res.json(cats)
   })
+
 
 
 
